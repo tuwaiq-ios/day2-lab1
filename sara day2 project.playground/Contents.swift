2f0a3773774f6d77d3d1cc9308a1e5cdda2a082d
sarasaud <unix.663@gmail.com>
2021-09-27 20:05:13 +0300

    lap1 in day2

---
+++ sara day2 project.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+var greeting = "Hello, playground"
+//Q1
+var a = 2
+var b = 4
+//Q2
+var sum1 = a+b
+print(sum1)
+//Q3
+var rainbowcolors:[String] =      ["red","white","blue","pink","black","brwon"]
+//Q4
+rainbowcolors.append("gray")
+//Q5
+print (rainbowcolors[4])
+//Q6
+rainbowcolors.remove(at: 1)
+//Q7
+var flowerskind: Set<String> = ["rose","iris","daisy","orcid"]
+//Q8
+for flowerskind in flowerskind {
+    print(flowerskind)
+}
+//Q9
+var (sa) = "SARASAUDH."
+//Q10
+print (sa)
+
+
