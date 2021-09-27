ff69cded059182b3f1d692df161aab6d86c93ec9
Ibrahim Asiri <i-m-6@hotmail.com>
2021-09-27 16:37:22 +0300

    Lab for Day2

---
+++ w1d2.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var a = 4
+var b = 6
+
+print(a+b)
+
+var arr = ["Ibrahim", "Asiri","27" ,"9 ", "2021", "Tuwaiq"]
+
+arr.append("lab")
+
+print(arr[3])
+
+arr.remove(at: 1)
+
+var day2 : Set = ["Array", "Set", "String", "Variables"]
+ print(day2)
+
+var my_name = "Ibrahim Asiri"
+
+print(my_name)
+
