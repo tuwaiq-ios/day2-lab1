b7518c762dc717101a3bd5ca3900f0f6bc9b9f46
amal <amal.141726@gmail.com>
2021-09-27 17:50:53 +0300

    finished the lap

---
+++ amal Playground.playground/Contents.swift
@@ -0,0 +1,19 @@
+
+
+var a: Int  = 2
+var b: Int  = 5
+var sum = a+b
+print(sum)
+
+var myarray :[Int] = [1 , 2 , 3, 4 , 5 , 6]
+myarray.append(7)
+print(myarray)
+print(myarray[3])
+myarray.remove(at : 1)
+print(myarray)
+
+var myset : Set = ["sunday,monday,tuseday,wensday"]
+print(myset)
+
+var myname: String = "Amal"
+print(myname)
