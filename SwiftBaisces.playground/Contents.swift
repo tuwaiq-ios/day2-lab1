911316ff120044251434fa9a841037120ce4ca8c
dmdm <deemamoh15@hotmail.com>
2021-09-27 17:38:01 +0300

    finished

---
+++ SwiftBaisces.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+//1create two integer variables a and b
+var a = 10
+var b = 10
+//2 Print the sum of these variable
+print (a+b)
+//3 Create array of 6 elements
+var array:[Int] = [10,20,30,40,50,60]
+//4 Add the value 7 to the array
+array.insert(70, at: 6)
+//5 Print the fourth element of the array
+print (array [4])
+//6 Delete the second element of the array
+array.remove(at: 2)
+//7 Create a set of 4 elements
+var studentID : Set = [112, 114, 116, 118, 115]
+//8 print everything in the set
+print("Student ID: \(studentID)")
+//9 Create a string variable that holds your name
+var name:String = "deema"
+//10 Print your name (the variable)
+print(name)
+
