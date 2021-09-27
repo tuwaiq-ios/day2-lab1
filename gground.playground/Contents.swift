42812a04741435e45acd600fdbc1c4638f04b836
PC <alahmryn5@gmail.com>
2021-09-27 23:57:58 +0300

    lab2

---
+++ gground.playground/Contents.swift
@@ -0,0 +1,16 @@
+
+
+var greeting = "Hello, playground"
+var s = 1
+var y = 3
+ var sum = s + y
+print(sum)
+var numbers = [1, 2, 3, 4, 5, 6]
+
+numbers.append(7)
+print(numbers[3])
+numbers.remove(at : 1 )
+var numet : Set = ["h", "n", "r", "u"]
+print(numet)
+var name = "muneerah"
+print(name)
