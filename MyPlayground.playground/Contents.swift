3097b1bf2e9e620a8f3fb8d59f297ecc5b1acca4
Abdulaziz <abdulaziz@MacBook-Air-bdalzyz.local>
2021-09-27 17:47:07 +0300

    Lab2 finished

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+import Foundation
+
+var greeting = "Hello, playground"
+
+var a = 3
+var b = 4
+print(a + b)
+
+var number = [1,2,3,4,5,6]
+number .append(7)
+
+print(number[3])
+
+number .remove(at: 1)
+
+var student:Set = [5,8,9,2]
+print(student)
+var myname="Abdulaziz"
+print(myname)
+
