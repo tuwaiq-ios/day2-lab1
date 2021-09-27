9872f5b0898e44d96d35e38504ea9a000afbc911
Macbook <macbook@Macbooks-MacBook.local>
2021-09-27 17:06:41 +0300

    lab2 home work

---
+++ waleedplay.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+//Create two integer variables a and b
+var a=10
+var b=7
+//Print the sum of these variable
+var sum=a+b
+print(sum)
+//Create array of 6 elements
+var arr=[0,1,2,3,4,5]
+//Add the value 7 to the array
+arr.append(6)
+//Print the fourth element of the array
+print(arr[3])
+//Delete the second element of the array
+arr.remove(at: 2)
+//Create a set of 4 elements
+var studentid : Set=[20,25,19,22]
+//Print every element in the set
+print(studentid)
+//Create a string variable that holds your name
+var myname="waleed"
+//Print your name (the variable)
+print(myname)
