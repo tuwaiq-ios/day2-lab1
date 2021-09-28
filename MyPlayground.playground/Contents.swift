e2c315e9e89c052cd6f998cee41481ce52e408e9
tasnim <tasneemjafsher@gmail.com>
2021-09-28 08:50:26 +0300

    Home Work

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+var greeting = "Hello, playground"
+var a=7
+var b=10
+
+print (a+b)
+
+var num:[Int] = [1, 2, 3, 4, 5, 6,]
+
+num.insert(7, at:6)
+
+print (num [4])
+
+num.remove (at:2)
+
+var names : Set = ["sara,tsnim,ruba,reem"]
+
+print("names:\names)")
+
+var name:String = "tsnim"
+
+print (name)
+
+
