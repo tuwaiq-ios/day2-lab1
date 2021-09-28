fac53b8cd6766f56edd84c0603e1fe1fca815c3a
maram <maarram36@gmail.com>
2021-09-28 08:37:41 +0300

    done

---
+++ maramplayground/lab2 copy.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var a = 4
+var b = 2
+print (a+b)
+
+var arrayx :[Int] = [1,2,3,4,5,6]
+arrayx.append(7)
+arrayx[3]
+arrayx.remove(at:1)
+
+var d : Set = [9, 8,1,3]
+print (d)
+var m : String = "maram"
+print (m)
+
