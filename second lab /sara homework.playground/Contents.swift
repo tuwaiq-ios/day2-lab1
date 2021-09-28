299f6848328d7f1c5e8b067a8920a01a3fc4fe25
sara al zhrani <saraalzhrani@MacBook-Pro-alkhas-b-sara.local>
2021-09-28 09:23:34 +0300

    Done

---
+++ second lab /sara homework.playground/Contents.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+var greeting = "Hello, playground"
+//Create two integer variables a and b
+
+var a = 2
+var b = 3
+//Print the sum of these variable
+
+var sum = a + b
+print (sum)
+
+//Create array of 6 elements
+
+
+var arraycolor =  ["red", "green", "white", "blue", "pink", "black" ]
+
+//Add the value 7 to the array
+
+
+var array = ["pruple"]
+
+arraycolor.append("pruple")
+
+print(arraycolor)
+//Print the fourth element of the array
+
+
+print(arraycolor [3])
+
+//Delete the second element of the array
+
+arraycolor.remove(at: 2)
+
+print(arraycolor)
+//Create a set of 4 elements
+
+var setnumbers =  [7, 8, 9, 10]
+ 
+//Print every element in the set
+
+ 
+print(setnumbers)
+
+//Create a string variable that holds your name
+ var myname = ("Sara Alzhrani")
+
+
+//Print your name (the variable)
+
+print (my name)
