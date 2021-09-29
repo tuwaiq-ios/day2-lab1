3a870a33d336bc45658e58f122c2c7809eb4be59
MacBook <macbook@MacBook-Air-MacBook.local>
2021-09-29 14:10:57 +0300

    finsh

---
+++ project day2/MyPlayground.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+var greeting = "Hello, playground"
+var a = 5
+var b = 5
+var sum = a+b
+print (sum)
+
+
+var myArray = [2, 4, 6, 8, 10, 12]
+myArray.append(7)
+print(myArray)
+print(myArray[3])
+myArray.remove(at: 1)
+print(myArray)
+
+
+var mySet = ["amal, lma, nora, alma"]
+print (mySet)
+
+var myName = "fatimah"
+print (myName)
+
+
+
+
+
